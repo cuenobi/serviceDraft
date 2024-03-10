cb2e0d8ae34062fcc39d2e24c9ebd628f9518d82
Sitthikon Khumtong <sitthikon.work@gmail.com>
2024-03-10 15:47:12 +0700

    modify

--- service/entity/user.go
+++ service/entity/user.go
@@ -6,23 +6,25 @@ import (
 )
 
 type User struct {
-	ID        string         `gorm:"primarykey;"`
-	DeletedAt gorm.DeletedAt `gorm:"index"`
-	Prefix    *string        `gorm:"default:null"`
-	Firstname *string        `gorm:"default:null"`
-	Lastname  *string        `gorm:"default:null"`
-	Fullname  *string        `gorm:"default:null"`
-	Phone     *string        `gorm:"default:null"`
-	Email     *string        `gorm:"default:null"`
-	Username  *string        `gorm:"default:null"`
-	Password  *string        `gorm:"default:null"`
-	Role      *string        `gorm:"default:null"`
+	ID           uint           `gorm:"primarykey;autoIncrement"`
+	Uuid         string         `gorm:"primarykey;"`
+	DeletedAt    gorm.DeletedAt `gorm:"index"`
+	Prefix       *string        `gorm:"default:null"`
+	Firstname    *string        `gorm:"default:null"`
+	Lastname     *string        `gorm:"default:null"`
+	Fullname     *string        `gorm:"default:null"`
+	Phone        *string        `gorm:"default:null"`
+	Email        *string        `gorm:"default:null"`
+	Username     *string        `gorm:"default:null"`
+	Password     *string        `gorm:"default:null"`
+	Role         *string        `gorm:"default:null"`
+	RefreshToken *string        `gorm:"default:null"`
 
 	// Custom detail
 }
 
 func (user *User) BeforeCreate(db *gorm.DB) (err error) {
 	uuid := uuid.New()
-	user.ID = uuid.String()
+	user.Uuid = uuid.String()
 	return nil
 }
